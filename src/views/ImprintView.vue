481c089748877331a0fd93b8722c14bbb26cbd6e
Kevin Schmidt <kevin.schmidt@student.htw-berlin.de>
2026-08-30 17:38:27 +0200

    ADD imprint

--- src/views/ImprintView.vue
+++ src/views/ImprintView.vue
@@ -4,9 +4,67 @@
 
 <template>
     <div class="content">
-        
+        <h1>Impressum</h1>
+        <span>Angaben gemäß §5 DDG</span>
+        <div class="imprintContent">
+            <div class="address">
+                <p>Kevin Schmidt</p>
+                <p>Sültstraße 27</p>
+                <p>10409 Berlin</p>
+                <p>Deutschland</p>
+            </div>
+        </div>
+        <h2>Kontakt:</h2>
+        <div class="imprintContent">
+            <div class="contact">
+                <p>E-Mail: <a href="mailto:2001kevin.schmidt@gmail.com">2001kevin.schmidt@gmail.com</a></p>
+            </div>
+        </div>
+        <hr/>
+        <h2>Haftung für Inhalte</h2>
+        <p>
+            Die Inhalte dieser Website wurden mit größtmöglicher Sorgfalt erstellt. Für die Richtigkeit, Vollständigkeit und Aktualität der bereitgestellten Inhalte kann jedoch keine Gewähr übernommen werden.
+        </p>
+        <p>
+            Als Diensteanbieter bin ich gemäß den allgemeinen gesetzlichen Vorschriften für eigene Inhalte auf dieser Website verantwortlich. Eine Verpflichtung zur Überwachung übermittelter oder gespeicherter fremder Informationen besteht jedoch nur im Rahmen der gesetzlichen Vorschriften.
+        </p>
+        <h2>Haftung für Links</h2>
+        <p>
+            Diese Website enthält gegebenenfalls Links zu externen Websites Dritter, auf deren Inhalte ich keinen Einfluss habe. Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber verantwortlich.
+        </p>
+        <p>
+            Zum Zeitpunkt der Verlinkung waren keine Rechtsverstöße erkennbar. Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar.
+        </p>
+        <h2>Urheberrecht</h2>
+        <p>
+            Die durch mich erstellten Inhalte und Werke auf dieser Website unterliegen dem deutschen Urheberrecht. Eine Vervielfältigung, Bearbeitung, Verbreitung oder sonstige Verwertung außerhalb der Grenzen des Urheberrechts bedarf meiner vorherigen Zustimmung, sofern nicht gesetzlich etwas anderes bestimmt ist.
+        </p>
+        <p>
+            Soweit Inhalte auf dieser Website nicht von mir erstellt wurden, werden die Rechte der jeweiligen Urheber beachtet.
+        </p>
     </div>
 </template>
 
 <style scoped>
+
+h1 {
+    margin-bottom: 0;
+}
+
+.imprintContent {
+    margin-left: 0;
+}
+@media (min-width: 600px) {
+    .imprintContent {
+        margin-left: 2em;
+    }
+}
+
+.address p {
+    margin: 0;
+}
+.address p:first-child {
+    margin-top: 1.5em
+}
+
 </style>
